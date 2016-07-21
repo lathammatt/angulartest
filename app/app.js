7c7389991033df90a9df282849980f97088742b8
Matt Latham <latham75@gmail.com>
2016-07-21 16:18:30 -0500

    removed firebase argument, fixed authconfig argument

--- app/app.js
+++ app/app.js
@@ -2,9 +2,18 @@
 
 var app = angular.module("ToDoApp", ['ngRoute'])
 	.constant('FirebaseURL', "https://todolistexercise.firebaseio.com/");
+
+
+
 	
 
-app.config(function($routeProvider){
+app.config(function($routeProvider, FBCreds){
+	let authConfig = {
+		apiKey: FBCreds.apiKey,
+		authDomain: FBCreds.authDomain
+	};
+	firebase.initializeApp(authConfig);
+
 	$routeProvider.
 		when('/items/list', {
 			templateUrl: 'partials/item-list.html',
